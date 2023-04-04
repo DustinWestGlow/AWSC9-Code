5e42edfff3e877a5cee620875eb8bb31f7c3ce5f
Dustin West <dustinwestglow@gmail.com>
2023-04-04 19:53:20 +0000

    stopping most of the cipher reformation. just going to make the affine cipher and move onto frequency analysis

--- ciphers/monoalphabetic/crack.py
+++ ciphers/monoalphabetic/crack.py
@@ -6,43 +6,11 @@
 
 from monoalphabetic import Monoalphabetic
 
-# message = "and you too, brutus?"
-key = 13
+# DONE! check 'caesar.txt' for results
 mono = Monoalphabetic()
-abcm = mono.alphabet
-mono.caesar(key) # mono.genmap("c", 3)
-encrypted = mono.substitute(abcm)
-again = mono.substitute(encrypted)
-print(key)
-print(abcm)
-print(encrypted)
-print(again)
-
-
-import random
-print(f"MESSAGE: '{message}'")
-print("ENCRYPTING...")
-multiplied = random.choice(m_ks)
-shifted = random.choice(c_ks)
-encrypted = mc.encrypt(message, multiplied)
-print(f"multiplicative '{multiplied}': {encrypted}")
-encrypted = caesar_cipher(encrypted, shifted)
-print(f"caesar '{shifted}': {encrypted}")
-
-print("DECRYPTING...")
-# decrypt attempts
-attack_file = open("attacks_affine.txt", "w")
-attack_file.write("shift,multiply,caesar,multiplicative,result\n")
-for mult in m_ks:
-    for shift in c_ks:
-        c = encrypted
-        m = caesar_cipher(c, shift)
-        k = mc.encrypt(m, mult)
-        p = k
-        attack_file.write(f"{shift},{mult},{m},{k},{p}\n")
-        if p == message:
-            print(f"caesar '{shift}': {m}")
-            print(f"multiplicative '{mult}': {k}")
-            print("CRACKED!")
-            print(f"message = '{p}'")
-attack_file.close()
+abcm = abcm = mono.alphabet
+caesar_file = open("caesar.txt", "w")
+for key in range(0, 26+1):
+    mono.caesar(key)
+    shifted = mono.substitute(abcm)
+    caesar_file.write(f"{key},{shifted}\n")
