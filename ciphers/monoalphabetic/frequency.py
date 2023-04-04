fee0a46475b945aea0feb6435f35d75efa02ea77
Dustin West <dustinwestglow@gmail.com>
2023-04-04 22:55:11 +0000

    finally got the dictionary comprehension solved for counting ciphertext letter frequencies. Now try english letter frequencies...

--- ciphers/monoalphabetic/frequency.py
+++ ciphers/monoalphabetic/frequency.py
@@ -1,33 +1,61 @@
 import csv
+import random
 
-
+# count frequency of letters in ciphertext
+# ciphertext = "uihjkcaldcbu" a.k.a. nonsense
 ciphertext = ""
 with open("ciphertext.txt") as reader:
     ciphertext = reader.read()
+# {'a': 0, 'b': 0, ...}
+abc = "".join([chr(x + ord('a')) for x in range(26)])
+zeros = [0] * len(abc)
+ct_count = dict(zip(abc, zeros))
+for c in ciphertext:
+    if not c.isalpha():
+        continue
+    ct_count[c.lower()] += 1
+# {'a': 5, 'b': 14, ...}
+# print(ct_count)
 
+# sort ciphertext letter frequencies (descending)
+# R, S, F, I...
+frequency_ciphertext = []
+# https://stackoverflow.com/questions/4859292/how-can-i-get-a-random-key-value-pair-from-a-dictionary
+# for sorting runs
+for i in range(len(ct_count)):
+    # find most abundant letter
+    abundant_letter = random.choice(list(ct_count.keys()))
+    for key in ct_count:
+        if ct_count[key] >= ct_count[abundant_letter]:
+            abundant_letter = key
+    # append it
+    frequency_ciphertext.append(abundant_letter)
+    # https://www.freecodecamp.org/news/python-remove-key-from-dictionary/
+    # remove it and repeat for next highest letters
+    del ct_count[abundant_letter]
+# print(frequency_ciphertext)
 
-
-# unsorted list of english letters and their frequencies
-uns_eng_frq = []
-# https://docs.python.org/3/library/csv.html
-with open('uns_eng_frq.csv', newline='') as csvfile:
-    reader = csv.reader(csvfile, delimiter=',')
-    for row in reader:
-        # 'a': 0.08167
-        letter = str(row[0]).lower()
-        frequency = float(row[1])
-        obj = {
-            letter: frequency
-        }
-        uns_eng_frq.append(obj)
-print(uns_eng_frq)
-# https://www.freecodecamp.org/news/sort-dictionary-by-value-in-python/#howtosortdatawiththesortedmethod
-# keep as list of tuples for our needs and purposes
-sorted_frequencies = sorted(uns_eng_frq) #, key=lambda x:x, reverse=True)
-with open("srt_eng_frq.csv", newline='') as csvfile:
-    writer = csv.writer(csvfile, delimiter=',')
-    for frq in sorted_frequencies:
-        writer.writerow([sorted_frequencies[0], sorted_frequencies[1]])
+# # unsorted list of english letters and their frequencies
+# uns_eng_frq = []
+# # https://docs.python.org/3/library/csv.html
+# with open('uns_eng_frq.csv', newline='') as csvfile:
+#     reader = csv.reader(csvfile, delimiter=',')
+#     for row in reader:
+#         # 'a': 0.08167
+#         letter = str(row[0]).lower()
+#         frequency = float(row[1])
+#         obj = {
+#             letter: frequency
+#         }
+#         uns_eng_frq.append(obj)
+# print(uns_eng_frq)
+# # https://www.freecodecamp.org/news/sort-dictionary-by-value-in-python/#howtosortdatawiththesortedmethod
+# # keep as list of tuples for our needs and purposes
+# sorted_frequencies = sorted(uns_eng_frq) #, key=lambda x:x, reverse=True)
+# with open("srt_eng_frq.csv", newline='') as csvfile:
+#     writer = csv.writer(csvfile, delimiter=',')
+#     for frq in sorted_frequencies:
+#         writer.writerow([sorted_frequencies[0], sorted_frequencies[1]])
 
     
 
