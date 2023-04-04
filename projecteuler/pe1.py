355efb7d4276bdcf8fcf78e04a6dfc1dc5edb315
Dustin West <dustinwestglow@gmail.com>
2023-04-04 18:28:41 +0000

    Refactored problem 1

--- projecteuler/pe1.py
+++ projecteuler/pe1.py
@@ -1,10 +1,16 @@
 # https://projecteuler.net/problem=1
 # Find the sum of all the multiples of 3 or 5 below 1000.
-limit = 10**3
-total = 0
 
-for i in range(1, limit):
-    if (i % 3 == 0) or (i % 5 == 0):
-        total += i
+stophere = 10**3
+sum_multiples = 0
 
-print(total)
+# O(n), iterate over every n
+# between 1 and upper bound
+for incrementer in range(1, stophere):
+    n = incrementer
+    # check if n is a multiplice of 3 or 5
+    if (n % 3 == 0) or (n % 5 == 0):
+        sum_multiples += n
+
+# DONE!
+print(sum_multiples)
